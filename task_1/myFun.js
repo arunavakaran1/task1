b68ee339bfbefb634e74ab4764dd605e9bffe31b
arunavakaran1 <arunava.karan@gmail.com>
2020-06-19 12:32:35 +0530

    add them all

---
+++ task_1/myFun.js
@@ -0,0 +1,14 @@
+console.log('karan');
+function red(){
+    alert('YOU CLICK ON RED');
+}
+function blue(){
+    alert('YOU CLICK ON BLUE');
+}
+function yellow(){
+    alert('YOU CLICK ON YELLOW');
+}
+function black(){
+    alert('YOU CLICK ON BLACK');
+}
+
